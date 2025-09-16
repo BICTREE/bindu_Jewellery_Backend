2c262ccf2940c769620efb400155d96711631adb
AnandhuRs619 <ananthurs619@gmail.com>
2025-09-16 17:08:43 +0530

    fix: Add comment for admin middleware in category routes

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -13,7 +13,7 @@ categoryRouter.get('/:id', getCategoryByIdCtrl);
 
 categoryRouter.use(authMiddleware)
 categoryRouter.use(roleChecker(['admin']))
-
+// admin
 categoryRouter.post('/', categoryValidator.create, validateMW, createCategoryCtrl);
 categoryRouter.put('/:id', categoryValidator.update, validateMW, updateCategoryCtrl);
 categoryRouter.patch('/:id', updateCategoryStatusCtrl);
