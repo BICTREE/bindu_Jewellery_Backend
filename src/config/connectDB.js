607b27bbee1a2e4b89805f088abba09326fb4e65
AnandhuRs619 <ananthurs619@gmail.com>
2025-09-22 17:27:32 +0530

    fix: Update MongoDB connection to use environment variable for URI

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
  
 export const connectDB = async () => {
     try {
-       const cloudDB = await mongoose.connect(
-  'mongodb+srv://ananthurs619_db_user:aTWm1VRvVRBJR8nJ@cluster0.jsb8nuy.mongodb.net/BinduTestDB?retryWrites=true&w=majority&appName=Cluster0'
-);
+       const cloudDB = await mongoose.connect(process.env.MongoURI);
 	console.log(`MongoDB Connected: ${cloudDB.connection.host}`);
 
     } catch (error) {
